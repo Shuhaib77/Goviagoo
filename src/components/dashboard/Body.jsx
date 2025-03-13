72947be85ab9a5a30a2d1f2a2f62bcab0f9796ee
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-03-13 16:02:18 +0530

    updated

--- src/components/dashboard/Body.jsx
+++ src/components/dashboard/Body.jsx
@@ -204,7 +204,7 @@ function Body() {
                           <p className="text-xs text-gray-700">
                             Hotel: {stay.name || "Not specified"}
                           </p>
-                        {stay.foodSpot.map((item) => (
+                        {stay?.foodSpot?.map((item) => (
                           <div>
                           <p className="text-sm font-semibold">
                             {index + 1}. {item.name} days stay
