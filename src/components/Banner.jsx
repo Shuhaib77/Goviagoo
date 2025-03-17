ba2ecc210a0426582dd74cc0e1f2948bd03ad494
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-03-17 12:18:07 +0530

    updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,64 +3,44 @@ import React from "react";
 const Banner = ({ image, searchbar }) => {
   return (
     <>
-      <div className="pl-9 pr-9   ">
+      <div className="px-9">
+        {/* Banner Section */}
         <div
           style={{ backgroundImage: `url(${image})` }}
-          className="h-[55vh] ml-8 mr-8  mt-10  bg-banner1 bg-center bg-cover bg-no-repeat rounded-2xl shadow-xl "
+          className="h-[55vh] mx-8 mt-10 bg-center bg-cover bg-no-repeat rounded-2xl shadow-xl"
         >
           {searchbar && (
-            <div className="flex lg:justify-end items-center sm:flex justify-center w-full p-5">
-              <h1>{searchbar.input}</h1>
-              <h1 className="position relative right-7">{searchbar.icon}</h1>
+            <div className="flex lg:justify-end items-center sm:justify-center w-full p-5 ">
+              <h1 >{searchbar.input}</h1>
+              <h1 className="relative right-7">{searchbar.icon}</h1>
             </div>
           )}
         </div>
-      <div>
-      <div className="lg:w-[120vh] relative   sm:w-full h-[80px] lg:p-4  m-auto rounded-3xl shadow-xl mb-10    bg-white  bottom-10  flex justify-around  items-center ">
+
+        {/* Bottom Bar (Fixed Positioning Issue) */}
+        <div className="lg:w-[120vh] sm:w-full h-[80px] lg:p-4 m-auto rounded-3xl shadow-xl bg-white -mt-8 flex justify-around items-center">
           <div className="flex justify-center items-center ml-3">
-            <div>
-              <h1>
-                <i
-                  class="fa-solid fa-location-dot fa-xl"
-                  style={{ color: " #232323" }}
-                ></i>
-              </h1>
-            </div>
-            <div className=" lg:ml-3 text-center">
-              <h1 className="text-sm">Go toMap</h1>
-              <h1 className="text-xs">where you are going?</h1>
+            <i className="fa-solid fa-location-dot fa-xl  text-gray-900"></i>
+            <div className="lg:ml-3 text-center">
+              <h1 className="text-sm">Go to Map</h1>
+              <h1 className="text-xs">Where are you going?</h1>
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <div>
-              <h5>
-                <i
-                  class="fa-solid fa-road-circle-check fa-xl"
-                  style={{ color: "#232323" }}
-                ></i>
-              </h5>
-            </div>
+            <i className="fa-solid fa-road-circle-check fa-xl text-gray-900"></i>
             <div className="text-center lg:ml-3">
-              <h1 className="text-sm">Create roadMap</h1>
-              <h1 className="text-xs">create custom map</h1>
+              <h1 className="text-sm">Create Roadmap</h1>
+              <h1 className="text-xs">Create a custom map</h1>
             </div>
           </div>
           <div className="flex justify-center items-center ml-3">
-            <div>
-              <h1>
-                <i
-                  class="fa-solid fa-map-location-dot fa-xl"
-                  style={{ color: "#232323" }}
-                ></i>
-              </h1>
-            </div>
+            <i className="fa-solid fa-map-location-dot fa-xl text-gray-900"></i>
             <div className="text-center lg:ml-3">
-              <h1 className="text-sm">Explre destinations</h1>
-              <h1 className="text-xs">find your destinations</h1>
+              <h1 className="text-sm">Explore Destinations</h1>
+              <h1 className="text-xs">Find your destinations</h1>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
